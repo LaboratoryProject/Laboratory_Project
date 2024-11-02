a138dbc313f81911308d2fd6063ea5893be4f6bb
AfraeAfr <afraeafrae01@gmail.com>
2024-11-02 13:37:09 +0100

    TestingMath afr

---
+++ Backend/analyse-service/src/test/java/com/laboratoire/analyse_service/MathServiceTest.java
@@ -0,0 +1,16 @@
+package com.laboratoire.analyse_service.service;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MathServiceTest {
+
+    private final MathService mathService = new MathService();
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, mathService.add(2, 3));
+        assertEquals(0, mathService.add(-1, 1));
+        assertEquals(-5, mathService.add(-2, -3));
+    }
+}
