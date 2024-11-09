af527e58f677b951dacae2a5d815c92612883503
hibatoun <hibamst59@gmail.com>
2024-11-09 16:14:05 +0100

    hibaasmst

--- Backend/analyse-service/src/test/java/com/laboratoire/analyse_service/MathServiceTest.java
+++ Backend/analyse-service/src/test/java/com/laboratoire/analyse_service/MathServiceTest.java
@@ -2,15 +2,8 @@ package com.laboratoire.analyse_service;
 
 import com.laboratoire.analyse_service.service.MathService;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
-@ActiveProfiles("local")
 public class MathServiceTest {
 
     private final MathService mathService = new MathService();
