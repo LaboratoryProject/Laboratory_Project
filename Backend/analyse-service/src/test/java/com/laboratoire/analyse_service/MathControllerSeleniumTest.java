dd56f4014c8f0b0f0f920bed2ecb4339c04028c5
hibatoun <hibamst59@gmail.com>
2024-11-22 13:59:47 +0100

    hiba's modificationsss

--- Backend/analyse-service/src/test/java/com/laboratoire/analyse_service/MathControllerSeleniumTest.java
+++ Backend/analyse-service/src/test/java/com/laboratoire/analyse_service/MathControllerSeleniumTest.java
@@ -29,7 +29,7 @@ public class MathControllerSeleniumTest {
 
         // Initialize the RemoteWebDriver with Selenium Hub URL and options
         webDriver = new RemoteWebDriver(
-                new URL("http://localhost:4444/ui"), // URL to Selenium Hub
+                new URL("http://localhost:4444"), // URL to Selenium Hub
                 options, false
         );
 
