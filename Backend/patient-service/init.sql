fc644056e35c6fa462a5cb27b69e9f0cd4c45b52
Oumaimaatr <aitramioumaima43@gmail.com>
2024-12-03 22:13:17 +0100

    oumaima microservice

---
+++ Backend/patient-service/init.sql
@@ -0,0 +1,2 @@
+CREATE DATABASE IF NOT EXISTS patient_service ;
+-- You can add more SQL commands here if needed, like table creation, user roles, etc.
---
+++ Backend/patient-service/src/test/java/com/laboratoire/patient_service/PatientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laboratoire.patient_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
