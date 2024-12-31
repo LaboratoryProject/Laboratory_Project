e16c5d567648ec67b67a5aad8359a5f1b197015c
hibatoun <hibamst59@gmail.com>
2024-12-31 16:10:09 +0100

    hibas 2 commit

---
+++ Backend/utilisateur-service/src/test/java/com/laboratoire/utilisateur_service/UtilisateurServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.laboratoire.utilisateur_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class UtilisateurServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
